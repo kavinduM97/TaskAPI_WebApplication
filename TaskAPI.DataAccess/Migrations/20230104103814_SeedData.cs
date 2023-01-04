1268533baa5950cdd1a12bab4d800679ce7d8356
kavinduM97 <kavindumedhanga@gmail.com>
2023-01-04 17:50:07 +0530

    dbconnection from first code method

---
+++ TaskAPI.DataAccess/Migrations/20230104103814_SeedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskAPI.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "Created", "Description", "Due", "Status", "Title" },
+                values: new object[] { 1, new DateTime(2023, 1, 4, 16, 8, 14, 757, DateTimeKind.Local).AddTicks(4374), "Get some text", new DateTime(2023, 1, 9, 16, 8, 14, 757, DateTimeKind.Local).AddTicks(4387), 0, "Get books from DB" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ TaskAPI.DataAccess/TodoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using WebApplication1.Models;
+
+namespace TaskAPI.DataAccess
+{
+    public class TodoDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+            var connectionSting = "Server=DESKTOP-UG59IH4\\SQLEXPRESS; Database=MyTodoDb;Trusted_Connection=True;Encrypt=False; User Id=sa; Password=admin";
+            optionsBuilder.UseSqlServer(connectionSting);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasData(new Todo
+            {
+                Id = 1,
+                Title = "Get books from DB",
+                Description = "Get some text",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+
+            });
+        }  
+    }
+}
